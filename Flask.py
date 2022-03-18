27ccc5ab4e459eba5d19623782ab5cc7feb4963d
IceScream-python <79906543+IceScream-python@users.noreply.github.com>
2022-03-18 12:05:38 +0100

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -4,24 +4,27 @@
 from flask import Flask, render_template, url_for,request
 import folium
 import random
+import sqlite3
+from requetes import Requetes
+
 app = Flask(__name__)   # Initialise l'application Flask
 
 start_coords = (48.9419981883, 2.61700200964)
 folium_map = folium.Map(location=start_coords, zoom_start=1)
 
-
 @app.route('/',methods=['GET','POST']) 
 def accueil():
     try: 
-        year = request.form['annee']
+        year = request.form['commune']
     except Exception as e:
-        print(e)
         year=' '
     coords = [random.randint(-180,180),random.randint(-84,84)]
     folium.Marker(coords, popup = str(coords)).add_to(folium_map)
-    return render_template("accueil.html",map=folium_map._repr_html_(),texte=str(year),annees = list(range(2010,2020,1)), commune = ['Grenoble','Paris'])
+    return render_template("accueil.html",map=folium_map._repr_html_(),texte=str(year),annees =  Requetes.renvoyer_liste('annee','ACCIDENTS_VELOS'), communes = Requetes.renvoyer_liste('Nom_dep','DEPARTEMENT') )
 
 
 if __name__ == '__main__' :
     app.run(debug=True)
-    
+
+cur.close()
+conn.close()
