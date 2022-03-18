04f3868af38fa7603686aeed53da9135dcd4dfa5
IceScream-python <79906543+IceScream-python@users.noreply.github.com>
2022-03-18 10:32:25 +0100

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 # -∗- coding: utf-8 -∗-
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request
 import folium
+import random
 app = Flask(__name__)   # Initialise l'application Flask
 
-@app.route('/') 
-def accueil():
-    return render_template("accueil.html")
+start_coords = (48.9419981883, 2.61700200964)
+folium_map = folium.Map(location=start_coords, zoom_start=1)
 
-@app.route('/map')
-def map():
-    start_coords = (46.9540700, 142.7360300)
-    folium_map = folium.Map(location=start_coords, zoom_start=14)
-    return folium_map._repr_html_()
- 
+
+@app.route('/',methods=['GET','POST']) 
+def accueil():
+    try: 
+        year = request.form['annee']
+    except Exception as e:
+        print(e)
+        year=' '
+    coords = [random.randint(-180,180),random.randint(-84,84)]
+    folium.Marker(coords, popup = str(coords)).add_to(folium_map)
+    return render_template("accueil.html",map=folium_map._repr_html_(),texte=str(year),annees = list(range(2010,2020,1)), commune = ['Grenoble','Paris'])
+
+
 if __name__ == '__main__' :
     app.run(debug=True)
+    
