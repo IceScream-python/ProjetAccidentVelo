8cf93cdc03081cd2d64c5876027139d5cc37f930
IceScream-python <79906543+IceScream-python@users.noreply.github.com>
2022-03-25 12:00:20 +0100

    Update and rename requetes to requetes.py

--- requetes.py
+++ requetes.py
@@ -1,19 +1,20 @@
 import sqlite3
-
 class Requetes():        
     @classmethod
     def renvoyer_liste(self,colonne,Table):
         conn = sqlite3.connect('Accident_France_2.db')
         cur = conn.cursor()
-        cur.execute(f"SELECT Distinct {colonne} FROM {Table}")
+        cur.execute(f"SELECT DISTINCT {colonne} FROM {Table}")
         conn.commit()
         villes = cur.fetchall()
-        print(villes[0])
         cur.close()
         conn.close()    
         return list(map(lambda x: x[0],villes))
     
-    @classmetod
+    @classmethod
     def liste_affichage(self,departement,annee,mois,jour,gravite):
         pass
     
+
+if __name__=='__main__':
+    print(Requetes.renvoyer_liste('Nom_dep','DEPARTEMENT'))
