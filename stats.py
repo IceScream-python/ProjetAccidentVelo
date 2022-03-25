f7c23d8786c02799d84d3c7f8ebe7965ca2a4bb9
IceScream-python <79906543+IceScream-python@users.noreply.github.com>
2022-03-25 12:02:43 +0100

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link href="{{ url_for('static',filename='mon_style.css')}}" rel="stylesheet" type"text/css">
+        <title>Licorne</title>
+    </head>
+    <body>
+        <a href="{{url_for('accueil')}}">Carte des accidents</a>
+        <p>
+            Hello World
+        </p>
+    </body>
+</html>
