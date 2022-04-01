6088dfdf19b1d65b4c4ebcb767a70a15d0642caa
lisa3846 <95339289+lisa3846@users.noreply.github.com>
2022-04-01 11:56:30 +0200

    Update requetes.py

--- requetes.py
+++ requetes.py
@@ -23,5 +23,5 @@ class Requetes():
         return resultat
         
 if __name__=='__main__':
-    liste_requete = {'annee':'2010','departement':'Ain','gravite':'2'}
-    print(Requetes.liste_affichage('lat, lon','ACCIDENTS_VELOS',f"WHERE annee={liste_requete['annee']} AND graviteaccident={liste_requete['gravite']}"))
+    liste_requete = {"annee":2010,"departement":"AUDE","gravite":"2"}
+    print(Requetes.liste_affichage('ACCIDENTS_VELOS.lat, ACCIDENTS_VELOS.lon','ACCIDENTS_VELOS',f"JOIN DEPARTEMENT ON ACCIDENTS_VELOS.num_dep=DEPARTEMENT.code WHERE annee={liste_requete['annee']} AND graviteaccident='{liste_requete['gravite']}' AND DEPARTEMENT.nom_maj='{liste_requete['departement']}'"))
