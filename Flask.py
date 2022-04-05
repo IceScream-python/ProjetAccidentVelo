712d477e5dc00a6f939a2b4bfda7cee24f3a81dc
IceScream-python <79906543+IceScream-python@users.noreply.github.com>
2022-04-05 10:12:57 +0200

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,34 +1,46 @@
-#!/usr/bin/python3
 # -∗- coding: utf-8 -∗-
 
+from tkinter import Variable
+from turtle import width
 from flask import Flask, render_template, url_for,request
 import folium
-#import random
+import folium.plugins
 import sqlite3
 from requetes import Requetes
 
 app = Flask(__name__)   # Initialise l'application Flask
 
 liste_requete=dict()
-start_coords = (48.9419981883, 2.61700200964)
-folium_map = folium.Map(location=start_coords, zoom_start=6)
+start_coords = (48.9419981883, 2.61700200964) #coordonées de Paris
+#folium_map = folium.Map(width=1680, height=750, location=start_coords, zoom_start=6) #Initialise une map folium
+markercluster = folium.plugins.FastMarkerCluster
 
 @app.route('/',methods=['GET','POST']) 
 def accueil():
+    folium_map = folium.Map(width=1680, height=750, location=start_coords, zoom_start=6) #Initialise une map folium
     for i in ['annee','departement','gravite']:
-        try:
-            liste_requete[i] = request.form[str(i)]
+        try:            
+            liste_requete[i] =  request.form[str(i)]
         except Exception as e:
-            liste_requete[i]=' '
-    #coords = [random.randint(-180,180),random.randint(-84,84)]
-    #folium.Marker(coords, popup = str(coords)).add_to(folium_map)
+            liste_requete[i]= None
+    try:
+        coordonees = Requetes.liste_affichage(liste_requete)
+        print(coordonees)
+        folium.plugins.MarkerCluster(coordonees).add_to(folium_map)
+
+    except Exception as e:
+       print(e)
+
+    
     print(liste_requete)
 
-    departements = Requetes.renvoyer_liste('Nom_dep','DEPARTEMENT')
-    gravites = Requetes.renvoyer_liste('graviteaccident','ACCIDENTS_VELOS')
-    annees = list(range(2008,2020,1))
-    #return render_template("accueil.html",map=folium_map._repr_html_(),texte=str(year),annees =  Requetes.renvoyer_liste('annee','ACCIDENTS_VELOS'), communes = Requetes.renvoyer_liste('Nom_dep','DEPARTEMENT') )
+    departements =  ['Tout']+ Requetes.renvoyer_liste('nom_dep')
+    gravites = ['Tout', 'Indemne','Blessé Léger', 'Blessé Hospitalisé','Tué']
+    annees =  ['Tout']+ Requetes.renvoyer_liste('annee')
+
+    #annees = list(range(2010,2020)) test avant d'avoir le sql
     return render_template("accueil.html",map=folium_map._repr_html_(),annees =  annees,departements = departements, gravites = gravites )
+
 
 @app.route('/stats',methods=['GET','POST'])
 def stats():
