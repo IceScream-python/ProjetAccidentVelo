6c08aad4db9d0298399c8b447cc362ada461d070
IceScream-python <79906543+IceScream-python@users.noreply.github.com>
2022-04-05 10:13:29 +0200

    Update requetes.py

--- requetes.py
+++ requetes.py
@@ -1,10 +1,11 @@
 import sqlite3
 class Requetes():
     @classmethod
-    def renvoyer_liste(self,colonne,Table):
+    def renvoyer_liste(self,colonne):
+        Table = {'annee':'ACCIDENTS_VELOS','nom_dep':'DEPARTEMENT','conditionsatmosperiques':'ACCIDENTS_VELOS','mois':'ACCIDENTS_VELOS'}
         conn = sqlite3.connect('Accident_France_2.db')
         cur = conn.cursor()
-        cur.execute(f"SELECT DISTINCT {colonne} FROM {Table}")
+        cur.execute(f"SELECT DISTINCT {colonne} FROM {Table[colonne]}")
         conn.commit()
         villes = cur.fetchall()
         cur.close()
@@ -12,16 +13,44 @@ class Requetes():
         return list(map(lambda x: x[0],villes))
 
     @classmethod
-    def liste_affichage(self,colonne,Table,parametre):
+    def liste_affichage(self,dico):
+        if None in dico.keys():
+            return []
         conn = sqlite3.connect('Accident_France_2.db')
         cur = conn.cursor()
-        cur.execute(f"SELECT DISTINCT {colonne} FROM {Table} {parametre}")
+        gravite = {'Indemne':0,'Blessé Léger':1, 'Blessé Hospitalisé':2,'Tué':3}
+        condition = ''
+        if dico['gravite'] != 'Tout':
+            condition += f" ACCIDENTS_VELOS.graviteaccident='{gravite[dico['gravite']]}'" 
+        if dico['departement'] != 'Tout':
+            if dico['gravite'] != 'Tout':
+                condition += ' AND'
+            condition += f" DEPARTEMENT.nom_dep='{dico['departement']}'"
+        if dico['annee'] != 'Tout':
+            if dico['gravite'] != 'Tout' or dico['departement'] != 'Tout':
+                condition += ' AND'
+            condition += f" ACCIDENTS_VELOS.annee={dico['annee']}"
+        if None in dico.keys():
+            return []
+        print(condition)
+        cur.execute(f"SELECT ACCIDENTS_VELOS.lat,ACCIDENTS_VELOS.lon, FROM ACCIDENTS_VELOS JOIN DEPARTEMENT ON ACCIDENTS_VELOS.departement = DEPARTEMENT.code  WHERE {condition} AND (ACCIDENTS_VELOS.lat<>0.0 AND ACCIDENTS_VELOS.lon<>0.0)")
         conn.commit()
         resultat = cur.fetchall()
         cur.close()
         conn.close()
         return resultat
-   
+    
+    @classmethod
+    def num_dep(self,nom_dep):
+        conn = sqlite3.connect('Accident_France_2.db')
+        cur = conn.cursor()
+        cur.execute(f"SELECT DISTINCT code FROM DEPARTEMENT WHERE nom_dep='{nom_dep}'")
+        conn.commit()
+        num = cur.fetchall()
+        cur.close()
+        conn.close()
+        return num[0][0]
+
     @classmethod
     def liste_stat(self,var):
         conn = sqlite3.connect('Accident_France_2.db')
@@ -53,7 +82,7 @@ class Requetes():
         conn.close()
         return liste
 
-        
 if __name__=='__main__':
-    liste_requete = {"annee":2010,"departement":"AUDE","gravite":"2"}
-    print(Requetes.liste_affichage('ACCIDENTS_VELOS.lat, ACCIDENTS_VELOS.lon','ACCIDENTS_VELOS',f"JOIN DEPARTEMENT ON ACCIDENTS_VELOS.num_dep=DEPARTEMENT.code WHERE annee={liste_requete['annee']} AND graviteaccident='{liste_requete['gravite']}' AND DEPARTEMENT.nom_maj='{liste_requete['departement']}'"))
+    print(Requetes.liste_affichage({'annee':'*','gravite':'Indemne','departement':'Ain'}))
+    #print(Requetes.renvoyer_liste('mois'))
+    #print(Requetes.num_dep('Ain'))
